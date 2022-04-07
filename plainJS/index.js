12645839c4289bc28033312c20c427e5692cdece
github username <github email address>
2022-04-07 20:07:26 +0100

    add state

--- plainJS/index.js
+++ plainJS/index.js
@@ -4,18 +4,59 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function isOperator(str) {
-  return str === "+" || str === "-" || str === "÷" || str === "=";
+  return (
+    str === "+" || str === "-" || str === "÷" || str === "=" || str === "x"
+  );
 }
+
+const initialState = {
+  firstValue: "",
+  operator: "",
+  secondValue: "",
+  screenValue: "0",
+};
+
+let state = initialState;
+
+const updateState = (e) => {
+  const str = e.target.textContent;
+  const isNumber = !isOperator(str);
+  switch (true) {
+    case str === "AC":
+      state = initialState;
+      break;
+    case isNumber && !state.operator:
+      const newFristValue = state.firstValue + str;
+      state = {
+        ...state,
+        firstValue: newFristValue,
+        screenValue: newFristValue,
+      };
+      break;
+    case isNumber && !!state.operator:
+      const newSecondValue = state.secondValue + str;
+      console.log("here");
+      state = {
+        ...state,
+        secondValue: newSecondValue,
+        screenValue: newSecondValue,
+      };
+      break;
+    case !isNumber:
+      state = { ...state, operator: str };
+      break;
+    default:
+      break;
+  }
+};
 
 function initialize() {
   const calculator = document.querySelectorAll(".buttonSection")[0];
   const screen = document.querySelectorAll(".screen")[0];
 
   const handleButtonClick = (e) => {
-    if (!isOperator(e.target.value)) {
-      console.log(e.target.value);
-      screen.textContent = e.target.textContent;
-    }
+    updateState(e);
+    screen.textContent = state.screenValue;
   };
   calculator.addEventListener("click", handleButtonClick);
 }
